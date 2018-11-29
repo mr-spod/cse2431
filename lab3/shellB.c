826d2e5f05f241236ae2493b23593e9bd488943c
mr-spod <sean.odonnell@duethealth.com>
2018-11-28 21:56:45 -0500

    dsf

--- lab3/shellB.c
+++ lab3/shellB.c
@@ -11,11 +11,11 @@ void scrubNewline(char *line) {
     line[strlen(line) - 1] = '\0';
 }
 
-void readHistory(char (*cmdHistory)[10][MAXLINE/2+1], int *cmdCount) {
+void readHistory(char (*cmdHistory[10])[MAXLINE/2+1], int *cmdCount) {
   int i, j;
   FILE *f;
   char *line;
-  char (*history)[MAXLINE/2+1];
+  char history[10][MAXLINE/2+1];
   size_t len = 0;
   ssize_t read;
 
