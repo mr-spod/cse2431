5f18f4589b5c90572deda07fb7ecbd16df14a44e
mr-spod <sean.odonnell@duethealth.com>
2018-11-28 21:54:42 -0500

    sh

--- lab3/shellB.h
+++ lab3/shellB.h
@@ -1,4 +1,4 @@
 #define MAXLINE 80 /* 80 chars per line, per command, should be enough. */
-void readHistory(char *cmdHistory[10][MAXLINE/2+1], int *cmdCount);
+void readHistory(char (*cmdHistory)[10][MAXLINE/2+1], int *cmdCount);
 
 void writeHistory(char cmdHistory[10][MAXLINE/2+1], int cmdCount);
