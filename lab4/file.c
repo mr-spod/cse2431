931f26adb7c87fa922dc1d244cbe9790467551d3
mr-spod <sean.odonnell@duethealth.com>
2018-12-09 22:19:10 -0500

    its actually working now

--- lab4/file.c
+++ lab4/file.c
@@ -11,70 +11,87 @@
 int matrixA[1200][1000];
 int matrixB[1000][500];
 int matrixC[1200][500];
+int matrixCSolution[1200][500];
 
 void *multiplyMatricesPortion(void *arg) {
-  int i, j, k, upperi, upperj, newval;
-  // fprintf(stderr, "hello again?\n");
+  int i, j, k, upperi, upperj, upperk;
   int *param = (int *) arg;
-  // fprintf(stderr, "param: %d\n", *param);
   int portion = *param % 10;
-  int numPortions = *param % 100;
-  i = (portion / numPortions) * 1200;
-  j = (portion / numPortions) * 500;
-  upperi = ((portion + 1) / numPortions) * 1200;
-  // printf("portion %d, upperi %d\n", portion, upperi);
-  upperj = ((portion + 1) / numPortions) * 500;
-  // printf("upperj %d\n", upperj);
+  int numPortions = *param / 10;
+  i = (1200 / numPortions) * portion;
+  j = (500 / numPortions) * portion;
+  k = (1000 / numPortions) * portion;
+  if (portion == (numPortions - 1)) {
+    upperi = 1200;
+    upperj = 500;
+    upperk = 1000;
+  } else {
+    upperi = (1200 / numPortions) * (portion + 1);
+    upperj = (500 / numPortions) * (portion + 1);
+    upperk = (1000 / numPortions) * (portion + 1);
+  }
 
   for (i; i < upperi; i++) {
     for (j; j < upperj; j++) {
+      matrixC[i][j] = 0;
       for (k = 0; k < 1000; k++) {
-        newval = matrixA[i][k] * matrixB[k][j];
-        if (matrixC[i][j] != newval && matrixC[i][j] != 0) {
-          printf("error: got a different value for same indices. old: %d, new: %d\n", matrixC[i][j], newval);
-          printf("i: %d, j: %d, k: %d\n", i, j, k);
-          printf("portion: %d, numPortions: %d\n", portion, numPortions);
-        }
-        matrixC[i][j] = matrixA[i][k] * matrixB[k][j];
+        matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
+      }
+      if (matrixC[i][j] != matrixCSolution[i][j]) {
+        fprintf(stdout, "error: incorrect value at row %d, column %d\n", i, j);
+        fprintf(stdout, "solution: %d\n", matrixCSolution[i][j]);
+        fprintf(stdout, "current: %d\n", matrixC[i][j]);
       }
     }
   }
+  fflush(stdout);
 }
 
 void initializeMatrices() {
   int i, j, k;
   for (i = 0; i < 1200; i++) {
+    for (k = 0; k < 1000; k++) {
+      matrixA[i][k] = i + k;
+    }
+  }
+  for (k = 0; k < 1000; k++) {
     for (j = 0; j < 500; j++) {
+      matrixB[k][j] = j;
+    }
+  }
+  for (i = 0; i < 1200; i++) {
+    for (j = 0; j < 500; j++) {
+      matrixC[i][j] = 0;
+      matrixCSolution[i][j] = 0;
       for (k = 0; k < 1000; k++) {
-        matrixA[i][k] = i + k;
-        matrixB[k][j] = j;
-        matrixC[i][j] = 0;
+        matrixCSolution[i][j] += matrixA[i][k] * matrixB[k][j];
       }
     }
   }
 }
 
 int main(void) {
-  int i, arg;
+  int i, *argP, arg;
   int n = 2;
-  pthread_t tid[6];
+  pthread_t *tid;
   pthread_attr_t attr;
   clock_t before, difference;
 
+  tid = malloc(6 * sizeof(pthread_t));
   pthread_attr_init(&attr);
-  // printf("attr init passed\n");
-  initializeMatrices();
-  // printf("initialized matrices\n");
-
+  arg = 0;
+  argP = &arg;
   for (n = 2; n <= 6; n++) {
+    initializeMatrices();
     printf("n = %d\n", n);
     before = clock();
     for (i = 0; i < n; i++) {
-      arg = n * 10;
-      arg += i;
-      int error = pthread_create(&tid[i], &attr, multiplyMatricesPortion, (void *) &arg);
+      argP = malloc(sizeof(int));
+      *argP = n * 10;
+      *argP += i;
+      int error = pthread_create(&(tid[i]), &attr, multiplyMatricesPortion, (void *) argP);
       if (error == 0)
-        printf("thread dispatch successfully\n");
+        printf("");
       else
         printf("Error %d: could not create thread\n", error);
     }
@@ -82,10 +99,11 @@ int main(void) {
       // printf("about to call pthread_join on thread %d of %d\n", i, n);
       int error = pthread_join(tid[i], NULL);
       if (error == 0)
-        printf("thread joined successfully\n");
+        printf("");
       else
         printf("Error %d: could not join thread %d of %d\n", error, i, n);
     }
+    free(argP);
     difference = clock() - before;
     printf("Running with %d threads took %lu seconds, %lu milliseconds\n", n, difference / 1000, difference % 1000);
   }
